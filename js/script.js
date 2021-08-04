143108ddc605e7c92f74832fec77fd291af02c8b
Leandro Jesus Do Nascimento <leandronasx18@gmail.com>
2021-08-03 22:16:24 -0300

    terceira commit

--- js/script.js
+++ js/script.js
@@ -5,15 +5,21 @@ arrayAcordeon.forEach(a => {
         this.classList.toggle("selecionado");
         const painel = this.nextElementSibling;
         let span = this.children;
+        span = span[0].children;
         span = span[0];
-        span = span.children;
         if(painel.style.maxHeight){
             painel.style.maxHeight = null;
+            span.classList.add("fa-plus");
+            span.classList.remove("fa-minus");
         }else{
             painel.style.maxHeight = painel.scrollHeight+"px";
+            span.classList.remove("fa-plus");
+            span.classList.add("fa-minus");
         }
     })
 });
+
+// ANO ATUAL
 
 
 let anoAtual = new Date;
